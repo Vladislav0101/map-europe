f061b3abbffdd7fa2e3666521b97c0915998c2b4
Vladislav0101 <by.vlad01@mail.ru>
2021-07-30 10:48:59 +0300

    feat: working with edits; router

--- src/router.js
+++ src/router.js
@@ -1,20 +1,41 @@
 import Vue from "vue";
 import VueRouter from 'vue-router'
 
+import Main from './pages/Main.vue'
 import Projects from './pages/Projects.vue'
+
+import First from './components/MainHeader/First.vue'
+import Second from './components/MainHeader/Second.vue'
+import Third from './components/MainHeader/Third.vue'
 
 Vue.use(VueRouter);
 
 const router = new VueRouter({
-  mode: "history",
+  mode: 'history',
   routes: [
     {
       name: "main",
+      path: '',
+      component: Main,
+      children: [
+        {
+          path: 'first',
+          component: First
+        },
+        {
+          path: 'second',
+          component: Second
+        },
+        {
+          path: 'third',
+          component: Third
+        }
+      ]
+    },
+    {
+      name: "projects",
       path: '/projects',
       component: Projects,
-      meta: {
-        requiresAuth: true
-      }
     },
   ]
 });
